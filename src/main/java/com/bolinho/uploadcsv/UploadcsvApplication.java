e511fde7039329e1925eb6119c0896fbc8e9a318
debora.brum <debrumneves@gmail.com>
2022-05-02 10:49:05 -0300

    first commit

---
+++ src/main/java/com/bolinho/uploadcsv/UploadcsvApplication.java
@@ -0,0 +1,13 @@
+package com.bolinho.uploadcsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadcsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadcsvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bolinho/uploadcsv/UploadcsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolinho.uploadcsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadcsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
