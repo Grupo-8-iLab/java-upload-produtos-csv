7368a5c3dc82f8b867aee47eaef42ac64d94dda2
lucasfpds <lucasfpd.santos@gmail.com>
2022-05-10 17:32:10 -0300

    tela para cadastrar

--- src/main/java/com/bolinho/uploadcsv/controller/ClienteController.java
+++ src/main/java/com/bolinho/uploadcsv/controller/ClienteController.java
@@ -7,10 +7,10 @@ import com.bolinho.uploadcsv.services.IClienteService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class ClienteController {
@@ -28,8 +28,15 @@ public class ClienteController {
     }
 
     @PostMapping("/clientes")
-    public String postClient(@RequestBody Cliente cliente) {
-        service.postOne(cliente);
-        return null;
+    public String postClient(Cliente cliente, Model model) {
+        try {
+            service.postOne(cliente);
+            model.addAttribute("message", "Cliente cadastrado com sucesso!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            model.addAttribute("message", "Erro ao salvar cliente: " + e.getMessage());
+        }
+        return "cliente";
     }
 }
