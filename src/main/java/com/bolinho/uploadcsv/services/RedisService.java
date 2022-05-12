70f197b0ef9b58f2536634ae3642cf7376cdebc6
debora.brum <debrumneves@gmail.com>
2022-05-12 15:40:50 -0300

    redis bug

--- src/main/java/com/bolinho/uploadcsv/services/RedisService.java
+++ src/main/java/com/bolinho/uploadcsv/services/RedisService.java
@@ -4,16 +4,15 @@ import java.util.List;
 import java.util.Set;
 
 import com.bolinho.uploadcsv.models.Cliente;
-import com.bolinho.uploadcsv.util.timeUnits;
 import com.google.gson.Gson;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.params.SetParams;
 
-@Component
+@Service
 public class RedisService {
     private Jedis jedis;
 
@@ -43,10 +42,21 @@ public class RedisService {
         return clientes;
     }
 
-    public void setInRedis() {
+    public List<Cliente> setDataInRedis() {
         List<Cliente> clientes = service.getAll();
-        this.writeArray("clientes", clientes);
-        // retorna a lista ??
+        System.out.println(clientes);
+        // this.writeArray("clientes", clientes);
+        return null;
+    }
+
+    public List<?> searchRedis() {
+        // busca no redis
+        Set<String> clientesRedis = this.read();
+        if (clientesRedis.isEmpty()) {
+            return this.setDataInRedis();
+        }
+        List<String> lista = List.copyOf(clientesRedis);
+        return lista;
     }
 
 }
